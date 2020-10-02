bfd319d14fcea141860dafafce79a59b897dd9fd
WasitShafi <wasitshafi700@gmail.com>
2020-10-03 00:22:11 +0530

    hacktober hack

--- Nearest Smaller Element.java
+++ Nearest Smaller Element.java
@@ -1,12 +1,29 @@
-/**
- *  @author WasitShafi
- *  @since 22-SEP-2020
- */
 
-import java.util.*;
-
-class TestClass {
-  public static void main(String[] args)
+      arr = new int[n];
+      ans = new int[n];
+      for (int i = 0; i < n; i++) {
+        arr[i] = scanner.nextInt();
+      }
+  
+      for (int i = 0; i < n; i++) {
+        ans[i] = arr[i];
+        for (int j = i - 1; j     arr = new int[n];
+        ans = new int[n];
+        for (int i = 0; i < n; i++) {
+          arr[i] = scanner.nextInt();
+        }
+    
+        for (int i = 0; i < n; i++) {
+          ans[i] = arr[i];
+          for (int j = i - 1; j     arr = new int[n];
+          ans = new int[n];
+          for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+          }
+      
+          for (int i = 0; i < n; i++) {
+            ans[i] = arr[i];
+            for (int j = i - 1; j public static void main(String[] args)
   {
     int n;
     int[] arr;
@@ -22,7 +39,15 @@ class TestClass {
 
     for (int i = 0; i < n; i++) {
       ans[i] = arr[i];
-      for (int j = i - 1; j >= 0 ; j--) {
+      for (int j = i - 1; j     arr = new int[n];
+      ans = new int[n];
+      for (int i = 0; i < n; i++) {
+        arr[i] = scanner.nextInt();
+      }
+  
+      for (int i = 0; i < n; i++) {
+        ans[i] = arr[i];
+        for (int j = i - 1; j >= 0 ; j--) {
         if (arr[j] < ans[i]) {
           ans[i] = arr[j];
           break;
@@ -40,4 +65,80 @@ class TestClass {
       }
     }
   }
+}    int[] ans;
+Scanner scanner = new Scanner(System.in);
+
+n = scanner.nextInt();
+arr = new int[n];
+ans = new int[n];
+for (int i = 0; i < n; i++) {
+  arr[i] = scanner.nextInt();
+}
+
+for (int i = 0; i < n; i++) {
+  ans[i] = arr[i];
+  for (int j = i - 1; j     arr = new int[n];
+  ans = new int[n];
+  for (int i = 0; i < n; i++) {
+    arr[i] = scanner.nextInt();
+  }
+
+  for (int i = 0; i < n; i++) {
+    ans[i] = arr[i];
+    for (int j = i - 1; j >= 0 ; j--) {
+    if (arr[j] < ans[i]) {
+      ans[i] = arr[j];
+      break;
+    }
+  }
+  if(ans[i] == arr[i]) {
+      ans[i] = -1;
+  }
+}
+
+for (int i = 0; i < n; i++) {
+  System.out.print(ans[i]);
+  if (i + 1 != n) {
+    System.out.print(" ");
+  }
+}
+}
+}    int[] ans;
+Scanner scanner = new Scanner(System.in);
+
+n = scanner.nextInt();
+arr = new int[n];
+ans = new int[n];
+for (int i = 0; i < n; i++) {
+  arr[i] = scanner.nextInt();
+}
+
+for (int i = 0; i < n; i++) {
+  ans[i] = arr[i];
+  for (int j = i - 1; j     arr = new int[n];
+  ans = new int[n];
+  for (int i = 0; i < n; i++) {
+    arr[i] = scanner.nextInt();
+  }
+
+  for (int i = 0; i < n; i++) {
+    ans[i] = arr[i];
+    for (int j = i - 1; j >= 0 ; j--) {
+    if (arr[j] < ans[i]) {
+      ans[i] = arr[j];
+      break;
+    }
+  }
+  if(ans[i] == arr[i]) {
+      ans[i] = -1;
+  }
+}
+
+for (int i = 0; i < n; i++) {
+  System.out.print(ans[i]);
+  if (i + 1 != n) {
+    System.out.print(" ");
+  }
+}
+}
 }
